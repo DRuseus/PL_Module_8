d926e3bded44e05353cad135fe90f7da276ea23e
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-08-16 16:47:01 +0300

    Сделано практическое задание по теме "Создание исключений".

---
+++ Module_8_3/module_8_3.py
@@ -0,0 +1,61 @@
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = '\033[31m' + message + '\033[0m'
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = '\033[31m' + message + '\033[0m'
+
+
+class Car:
+    __LEN_OF_NUMS = 6
+
+    def __init__(self, model, __vin, __numbers):
+        self.model = model
+        if self.__is_valid_vin(__vin):
+            self.__vin = __vin
+        if self.__is_valid_numbers(__numbers):
+            self.__numbers = __numbers
+
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        if 1000000 > vin_number or 9999999 < vin_number:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return True
+
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        if not len(numbers) == self.__LEN_OF_NUMS:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        return True
+
+
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
